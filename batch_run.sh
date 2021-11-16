ed077a76a11ba4e4b12398d2447f5029c65d83b1
gregglickert <gglick9@gmail.com>
2021-11-16 10:26:30 -0600

    thing i figured it out

--- batch_run.sh
+++ batch_run.sh
@@ -8,7 +8,7 @@
 #SBATCH --time 0-12:00
 
 START=$(date)
-mpiexec nrniv -mpi -quiet -python run_network.py simulation_configECP_base_edge_effects.json
+mpiexec nrniv -mpi -quiet -python run_network.py simulation_config.json
 END=$(date)
 
 { printf "Start: $START \nEnd:   $END\n" & python analysis.py --save-plots & printf "\n\n" & git diff components/synaptic_models/; }| mail -r gregglickert@mail.missouri.edu -s "AmygdalaTheta Simulation Results" -a analysis.png gregglickert@mail.missouri.edu
